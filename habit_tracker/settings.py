f2c17ff18c709dc003980e64ca3770e21adf17f4
Tyler Grantham <tjaygrantham@gmail.com>
2020-07-02 13:30:12 -0400

    Add apps

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 
     # Project-specific
     'users',
+    'core',
 ]
 
 MIDDLEWARE = [
