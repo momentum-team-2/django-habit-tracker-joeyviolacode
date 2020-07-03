a553a7bcf8a150e60c7d7832693351ca4377a189
joeyviolacode <joeyviolacode@gmail.com>
2020-07-03 17:06:58 -0400

    Set up base.html with a header and login functionality.  Added URLs and blanks to views.  Have a list_habit page with logic to display habits and records...placeholder <p>s.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Habit, Record
+from users.models import User
 
 # Create your views here.
+def list_habits(request):
+    user = request.user
+    habits = user.habits.all()
+    return render(request, 'core/list_habits.html', { "habits" : habits })
+
+def show_habit(request, pk):
+    pass
+
+def add_habit(request):
+    pass
+
+def edit_habit(request, pk):
+    pass
+
+def add_record(request):
+    pass
